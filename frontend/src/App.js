d24d0eb66b85672116863cb02531b51ec79759eb
admin <your.email@example.com>
2024-09-18 16:08:01 +0530

    login setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,22 @@
-
 import './App.css';
 import Login from './Login page/Login';
-
-
-
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+// import Home from './Home'; // Example: Add Home component
 
 function App() {
   return (
-   <>
-   <Login/>
-  
-   
-   
-   </>
+    <>
+      <BrowserRouter>
+        
+        <Routes>
+          
+          <Route path="/login" element={<Login />} />
+          
+          
+    
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
