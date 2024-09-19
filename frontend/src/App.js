65c506940b081ba0cc6a5bfdc211ece199e8e3c8
durgeshru <durgeshkumar7433@gmail.com>
2024-09-19 01:30:13 -0700

    nes code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import SuperAdmin from './component/Admin Dashboard/SuperAdmin';
 import Login from './Login page/Login';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 // import Home from './Home'; // Example: Add Home component
@@ -7,6 +8,7 @@ function App() {
   return (
     <>
       <BrowserRouter>
+      <SuperAdmin/>
         
         <Routes>
           
